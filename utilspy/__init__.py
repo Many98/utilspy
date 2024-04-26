798f1fa99db8ff6877e8585da91f2112a969b45d
Emanuel <65658910+Many98@users.noreply.github.com>
2024-04-26 13:11:49 +0200

    Update __init__.py

--- utilspy/__init__.py
+++ utilspy/__init__.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from utilspy import data_sample_to_excel, sql_excel, translate
+from utilspy import sql_excel, translate
 from utilspy.Connector import Connector
 
 
